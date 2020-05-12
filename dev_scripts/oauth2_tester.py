4e4fe39160c2997cdd21ec552283ef19a4a92a15
Zeev Rotshtein <zeevro@gmail.com>
2020-05-12 10:38:37 +0300

    Wicked tester script :)

--- dev_scripts/oauth2_tester.py
+++ dev_scripts/oauth2_tester.py
@@ -1,24 +1,53 @@
+import json
+
 from requests_oauthlib import OAuth2Session
 
-from flask import Flask, request, redirect, session
-from flask.json import jsonify
+from flask import Flask, request, render_template_string, session, url_for
+
+from wolf_iot.app import app as wolf_iot_app
 
 
 BASE_URL = 'https://localhost:3459/'
 AUTH_URL = BASE_URL + 'oauth/authorize'
 TOKEN_URL = BASE_URL + 'oauth/token'
 
-CLIENT_ID = 'asdfqwer'
-CLIENT_SECRET = 'asdfqwer'
+CLIENT_ID = wolf_iot_app.config['CLIENT_ID']
+CLIENT_SECRET = wolf_iot_app.config['CLIENT_SECRET']
+
+HTML_TEMPLATE = '''<html>
+    <head>
+        <title>Oauth2 response</title>
+        <link href="https://unpkg.com/prismjs@v1.x/themes/prism.css" rel="stylesheet" />
+        <script src="https://unpkg.com/prismjs@1.x/components/prism-core.min.js"></script>
+        <script src="https://unpkg.com/prismjs@v1.x/plugins/autoloader/prism-autoloader.min.js"></script>
+    </head>
+    <body>
+        <h3>Token</h3>
+        <pre><code class="language-json">{{ token|pretty_json|safe }}</code></pre>
+        <h3>Sync response</h3>
+        <pre><code class="language-json">{{ sync_resp|pretty_json|safe }}</code></pre>
+        <a href="/">Go again</a>
+    </body>
+</html>'''
 
 
 app = Flask(__name__)
 app.secret_key = b'P\x86\xecGV&\xbdm\x08z\xf2h}\xf6\x1dt\xc3vM\x06\x0b\xe6\x88\x9c.\xab\xa4\xbb\x84\x85\xbd\xc0'
 
 
+def pretty_json(o):
+    return json.dumps(
+        o,
+        indent=4,
+        sort_keys=True,
+        separators=(',', ': '),
+    )
+app.add_template_filter(pretty_json)
+
+
 @app.route("/")
 def login():
-    sess = OAuth2Session(CLIENT_ID, redirect_uri='https://localhost:8080/callback')
+    sess = OAuth2Session(CLIENT_ID, redirect_uri=url_for('callback', _external=True))
     authorization_url, state = sess.authorization_url(AUTH_URL)
 
     # State is used to prevent CSRF, keep this for later.
@@ -30,19 +59,34 @@ def login():
 def callback():
     sess = OAuth2Session(CLIENT_ID, state=session['oauth_state'])
     sess.verify = False
-    token = sess.fetch_token(TOKEN_URL, client_secret=CLIENT_SECRET, authorization_response=request.url)
+    token = sess.fetch_token(
+        TOKEN_URL,
+        include_client_id=True,
+        client_secret=CLIENT_SECRET,
+        authorization_response=request.url,
+    )
 
-    return jsonify(token)
+    sync_data = {
+        "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
+        "inputs": [{
+            "intent": "action.devices.SYNC"
+        }]
+    }
+
+    print(token)
+
+    sync_resp = sess.post(BASE_URL + 'api/fulfillment', json=sync_data).json()
+
+    return render_template_string(HTML_TEMPLATE, token=token, sync_resp=sync_resp)
+
+    return f'<h3>Token</h3><pre>{json.dumps(token, indent=2)}</pre><h3>Sync response</h3><pre>{json.dumps(sync_resp, indent=2)}</pre>'
 
 
 def main():
     app.run(
         '127.0.0.1',
         8080,
-        ssl_context=(
-            r'C:\Users\zeevr\Documents\home.zeevro.com-cert-and-chain.pem',
-            r'C:\Users\zeevr\Documents\home.zeevro.com-key.pem'
-        ),
+        ssl_context='adhoc',
         debug=True,
     )
 
